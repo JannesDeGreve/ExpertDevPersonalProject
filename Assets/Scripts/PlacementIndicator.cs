4f5672f9ecddc08311dcaaeed3023ece5776eeda
Jannes De Greve <jannes.de.greve@student.howest.be>
2019-11-21 17:54:29 +0100

    New floor marker, noodles, basic UI setup

--- Assets/Scripts/PlacementIndicator.cs
+++ Assets/Scripts/PlacementIndicator.cs
@@ -8,9 +8,21 @@ public class PlacementIndicator : MonoBehaviour
 {
     private ARRaycastManager rayManager;
     private GameObject visual;
+    bool isCreated = false;
+
+    //private GameObject variableForPrefab;
+    //public GameObject myPrefab;
+
+    private GameObject foodPrefab;
+    
 
     void Start ()
     {
+
+        foodPrefab = (GameObject)Resources.Load("prefabs/NoodleMeshCleaned", typeof(GameObject));
+
+        //foodPrefab = Instantiate(Resources.Load("prefabs/cube", typeof(GameObject))) as GameObject;
+
         // get the components
         rayManager = FindObjectOfType<ARRaycastManager>();
         visual = transform.GetChild(0).gameObject;
@@ -18,6 +30,7 @@ public class PlacementIndicator : MonoBehaviour
         // hide the placement indicator visual
         visual.SetActive(false);
     }
+
     
     void Update ()
     {
@@ -31,9 +44,54 @@ public class PlacementIndicator : MonoBehaviour
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
 
+            // foodPrefab.transform.position = hits[0].pose.position;
+            // foodPrefab.transform.rotation = hits[0].pose.rotation;
+
             // enable the visual if it's disabled
-            if(!visual.activeInHierarchy)
-                visual.SetActive(true);
+             if(isCreated == false)
+                    {
+                        // foodPrefab.transform.position = hitPose.position;
+                        // foodPrefab.transform.rotation = hitPose.rotation;
+                        // isCreated = true;
+                        visual.SetActive(true);
+                    }
+            // if(!visual.activeInHierarchy)
+            //     visual.SetActive(true);
+
+              if (Input.touchCount > 0)
+                {
+
+                    var hitPose = hits[0].pose;
+
+                    if(isCreated == false)
+                    {
+                        Instantiate(foodPrefab, hitPose.position, hitPose.rotation);
+
+                        // foodPrefab.transform.position = hitPose.position;
+                        // foodPrefab.transform.rotation = hitPose.rotation;
+                        isCreated = true;
+                    }
+                    else
+                    {
+                        //foodPrefab.SetActive(true);
+                        visual.SetActive(false);
+
+
+                    }
+                    //  if (foodPrefab == null)
+                    //     {
+                    //         Instantiate(foodPrefab, hitPose.position, hitPose.rotation);
+                    //     }
+                    //     else
+                    //     {
+                    //         visual.SetActive(false);
+
+                    //     }
+
+                    //Touch touch = Input.GetTouch(0);
+                    //touchPosition = Input.getTouch(0).position
+
+                }
         }
     }
 }
