4c088f8bb421c8853f92c7004021443a60d401e3
Jannes De Greve <jannes.de.greve@student.howest.be>
2019-12-12 21:01:09 +0100

    fixed all bugs, swipe to cycle between dishes

--- Assets/Scripts/ARDishSelectionMenu.cs
+++ Assets/Scripts/ARDishSelectionMenu.cs
@@ -16,22 +16,65 @@ public class ARDishSelectionMenu : MonoBehaviour {
 
     public Button restartScanButton;
 
-    public static KeyValuePair<string, string> selectedDish;
+    // public static KeyValuePair<string, string> selectedDish;
+    public static string selectedDish;
+    public static string selectedPath;
+
+    // public class dishesList {
+    //     string[] dishObject;
+    // }
+
+    // public class DishObject {
+    //     string dishName;
+    //     string dishPath;
+    // }
+    [System.Serializable]
+    public class testArrayData {
+        public string testDishName { get; set; }
+        public string testDishPath { get; set; }
+    }
+
+    public static string[][] dishObjectsArray;
+
+    public static List<testArrayData> newDishObjectArray;
+    public static int selectedIndex;
+    //public string[] dishObject;
 
     void Start () {
 
+        Debug.Log (OCRMain.matchedDishesWithPath.Count);
+        Debug.Log ("ik ben tot lijn 36 geraakt");
+
+        dishObjectsArray = new string[OCRMain.matchedDishesWithPath.Count][];
+        Debug.Log ("ik ben tot lijn 39 geraakt");
+
+        newDishObjectArray = new List<testArrayData> ();
+
         restartScanButton.onClick.AddListener (handleClickRestart);
+        Debug.Log ("ik ben tot lijn 42 geraakt");
+
+        int index = 0;
 
         foreach (KeyValuePair<string, string> keyValue in OCRMain.matchedDishesWithPath) {
-            Debug.Log (keyValue.Key);
+            Debug.Log ("ik ben tot in de foreach geraakt");
+
+            newDishObjectArray.Add (new testArrayData () {
+                testDishName = keyValue.Key,
+                    testDishPath = keyValue.Value
+            });
+
             GameObject button = (GameObject) Instantiate (buttonPrefab);
             Debug.Log (button);
-            button.GetComponentInChildren<Text> ().text = keyValue.Key;
+            // button.GetComponentInChildren<Text> ().text = keyValue.Key;
+            button.GetComponentInChildren<Text> ().text = newDishObjectArray[index].testDishName;
 
             button.transform.SetParent (menuPanel.transform, false);
 
             button.GetComponent<Button> ().onClick.AddListener (() => handleClickButton (keyValue));
+            index += 1;
         }
+
+        Debug.Log ("dish object array count: " + dishObjectsArray.Length);
 
     }
 
@@ -39,9 +82,20 @@ public class ARDishSelectionMenu : MonoBehaviour {
         SceneManager.LoadScene ("OCRMain");
     }
 
-    void handleClickButton (KeyValuePair<string, string> dish) {
-        selectedDish = dish;
-        Debug.Log (selectedDish);
+    void handleClickButton (KeyValuePair<string, string> keypair) {
+
+        int testIndex = 0;
+
+        Debug.Log ("naam" + keypair.Key);
+
+        foreach (var item in newDishObjectArray) {
+            if (item.testDishName == keypair.Key) {
+                selectedIndex = testIndex;
+            }
+            testIndex++;
+        }
+
+        Debug.Log ("de key is: " + selectedIndex + newDishObjectArray[selectedIndex].testDishName);
         SceneManager.LoadScene ("Main");
     }
 
--- Assets/Scripts/PlacementIndicator.cs
+++ Assets/Scripts/PlacementIndicator.cs
@@ -10,7 +10,7 @@ using UnityEngine.XR.ARSubsystems;
 
 public class PlacementIndicator : MonoBehaviour {
 
-    AssetBundle myLoadedAssetBundle;
+    private AssetBundle bundle;
     private string url = "http://jannesdegreve.be/assetbundles";
 
     private ARRaycastManager rayManager;
@@ -22,6 +22,9 @@ public class PlacementIndicator : MonoBehaviour {
     private string path;
     private string dishName;
     private string dishNameToRender;
+    public int indexToRenderVariable;
+
+    bool DishIsLoading;
 
     private GameObject biefstuk;
     private GameObject lang;
@@ -31,75 +34,29 @@ public class PlacementIndicator : MonoBehaviour {
 
     public List<GameObject> objectArray;
     public GameObject objectToRender;
+    private GameObject objectInstance;
+
+    private IEnumerator coroutine;
+
+    private void Awake () {
+        SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
+    }
 
     void Start () {
 
+        indexToRenderVariable = ARDishSelectionMenu.selectedIndex;
+
+        coroutine = loadSelectedObjectFromServer (indexToRenderVariable);
+        StartCoroutine (coroutine);
+
+        // Debug.Log ("selected in main: " + selectedDish);
+        // Debug.Log ("index in array: " + dishObjectsArray.indexOf (dishObjectsArray, [selectedDish, selectedPath]));
+
         // static var
         //Debug.Log (ARDishSelectionMenu.selectedDish);
-        Debug.Log ("Key: " + ARDishSelectionMenu.selectedDish.Key);
+        // Debug.Log ("Key: " + ARDishSelectionMenu.selectedDish[0]);
 
-        dishName = ARDishSelectionMenu.selectedDish.Key;
-        path = ARDishSelectionMenu.selectedDish.Value;
-        dishText.text = dishName;
-
-        Debug.Log ("voor operatie " + path);
-
-        dishNameToRender = path.Substring (path.IndexOf ('/') + 1);
-        dishNameToRender = dishNameToRender.Substring (dishNameToRender.IndexOf ('/') + 1);
-
-        Debug.Log ("dish name to render: " + dishNameToRender);
-
-        StartCoroutine ("loadSelectedObjectFromServer");
-
-        // foodPrefab = (GameObject) Resources.Load ("prefabs/NoodleMeshCleaned", typeof (GameObject));
-        // cubePrefab = (GameObject) Resources.Load ("prefabs/Cube", typeof (GameObject));
-        // biefstuk = (GameObject) Resources.Load ("prefabs/Biefstuk", typeof (GameObject));
-        // lang = (GameObject) Resources.Load ("prefabs/Lang", typeof (GameObject));
-        // lasagne = (GameObject) Resources.Load ("prefabs/Lasagne", typeof (GameObject));
-        // spaghetti = (GameObject) Resources.Load ("prefabs/Spaghetti", typeof (GameObject));
-        // lemons = (GameObject) Resources.Load ("prefabs/Lemons", typeof (GameObject));
-
-        // objectArray.Add (biefstuk);
-        // objectArray.Add (lang);
-        // objectArray.Add (lasagne);
-        // objectArray.Add (spaghetti);
-        // objectArray.Add (lemons);
-        // objectArray.Add (loadedObj);
-
-        // foreach (GameObject food in objectArray) {
-        //     food.transform.localScale = new Vector3 (0.05f, 0.05f, 0.05f);
-        // }
-
-        //objectToRender = spaghetti;
-
-        // matchedDishes.Add ("Spaghetti Bolognese");
-        // matchedDishes.Add ("Biefstuk met frieten");
-        // matchedDishes.Add ("Lasagne");
-        // matchedDishes.Add ("Een gerecht met een lange naam");
-
-        // switch (ARDishSelectionMenu.selectedDish) {
-        //     case "Spaghetti Bolognese":
-        //         Debug.Log ("Spaghetti");
-        //         objectToRender = spaghetti;
-        //         break;
-        //     case "Biefstuk met frieten":
-        //         Debug.Log ("Biefstuk");
-        //         objectToRender = biefstuk;
-        //         break;
-        //     case "Lasagne":
-        //         Debug.Log ("Lasagne");
-        //         objectToRender = lasagne;
-        //         break;
-        //     case "Een gerecht met een lange naam":
-        //         Debug.Log ("Lange naam");
-        //         objectToRender = lang;
-        //         break;
-        //     default:
-        //         Debug.Log ("Geen gerecht, code klopt niet foei");
-        //         break;
-        // }
-
-        //foodPrefab = Instantiate(Resources.Load("prefabs/cube", typeof(GameObject))) as GameObject;
+        //StartCoroutine ("loadSelectedObjectFromServer");
 
         // get the components
         rayManager = FindObjectOfType<ARRaycastManager> ();
@@ -110,7 +67,20 @@ public class PlacementIndicator : MonoBehaviour {
 
     }
 
-    IEnumerator loadSelectedObjectFromServer () {
+    IEnumerator loadSelectedObjectFromServer (int indexToRender) {
+        DishIsLoading = true;
+
+        dishName = ARDishSelectionMenu.newDishObjectArray[indexToRender].testDishName;
+        dishText.text = dishName;
+
+        path = ARDishSelectionMenu.newDishObjectArray[indexToRender].testDishPath;
+
+        Debug.Log ("voor operatie " + path);
+
+        dishNameToRender = path.Substring (path.IndexOf ('/') + 1);
+        dishNameToRender = dishNameToRender.Substring (dishNameToRender.IndexOf ('/') + 1);
+
+        Debug.Log ("dish name to render: " + dishNameToRender);
 
         string fullUrl = url + path;
         Debug.Log ("full url: " + fullUrl);
@@ -123,13 +93,61 @@ public class PlacementIndicator : MonoBehaviour {
             Debug.Log (request.downloadHandler);
         }
 
-        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent (request);
-        GameObject loadedObject = bundle.LoadAsset<GameObject> (dishNameToRender);
+        bundle = DownloadHandlerAssetBundle.GetContent (request);
+        objectToRender = bundle.LoadAsset<GameObject> (dishNameToRender);
 
-        Debug.Log (loadedObject == null ? "Failed to load assetBundle" : "AssetBundle succesfully loaded");
-        objectToRender = loadedObject;
+        Debug.Log (objectToRender == null ? "Failed to load assetBundle" : "AssetBundle succesfully loaded");
+        //objectToRender = loadedObject;
+        Debug.Log (objectToRender.name);
+
         objectToRender.transform.localScale = new Vector3 (0.05f, 0.05f, 0.05f);
+        DishIsLoading = false;
 
+    }
+
+    private void SwipeDetector_OnSwipe (SwipeData data) {
+        Debug.Log ("Swipe in Direction: " + data.Direction);
+
+        if (data.Direction.ToString () == "Left") {
+
+            if (indexToRenderVariable < ARDishSelectionMenu.newDishObjectArray.Count - 1) {
+                indexToRenderVariable++;
+                Debug.Log ("Left swipe, doe ++");
+            } else {
+                indexToRenderVariable = 0;
+                Debug.Log ("Left swipe, reset naar 0");
+            }
+
+            DishIsLoading = true;
+
+            Destroy (objectInstance);
+            bundle.Unload (true);
+
+            isCreated = false;
+            coroutine = loadSelectedObjectFromServer (indexToRenderVariable);
+            StartCoroutine (coroutine);
+
+        }
+
+        if (data.Direction.ToString () == "Right") {
+            if (indexToRenderVariable > 0) {
+                indexToRenderVariable--;
+                Debug.Log ("Right swipe, doe --");
+            } else {
+                indexToRenderVariable = ARDishSelectionMenu.newDishObjectArray.Count - 1;
+                Debug.Log ("Right swipe, reset naar lengte -1");
+            }
+
+            DishIsLoading = true;
+
+            Destroy (objectInstance);
+            bundle.Unload (true);
+
+            isCreated = false;
+            coroutine = loadSelectedObjectFromServer (indexToRenderVariable);
+            StartCoroutine (coroutine);
+
+        }
     }
 
     void Update () {
@@ -153,22 +171,23 @@ public class PlacementIndicator : MonoBehaviour {
         } else {
             visual.SetActive (false);
         }
-        // if(!visual.activeInHierarchy)
-        //     visual.SetActive(true);
 
-        if (Input.touchCount > 0) {
-            Touch touch = Input.GetTouch (0);
-            var hitPose = hits[0].pose;
-            instructionText.text = "";
+        if (DishIsLoading == false) {
+            if (Input.touchCount > 0) {
+                Touch touch = Input.GetTouch (0);
+                var hitPose = hits[0].pose;
+                instructionText.text = "Swipe om andere \ngerechten te zien.";
 
-            if (touch.phase == TouchPhase.Ended) {
+                if (touch.phase == TouchPhase.Ended) {
 
-                if (isCreated == false) {
-                    Instantiate (objectToRender, hitPose.position, hitPose.rotation);
-                    isCreated = true;
+                    if (isCreated == false) {
+                        objectInstance = (GameObject) Instantiate (objectToRender, hitPose.position, hitPose.rotation);
+                        isCreated = true;
+                    }
                 }
             }
         }
+
         // }
     }
 }
